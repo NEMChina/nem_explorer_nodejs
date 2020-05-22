90175900da867f40d166b0e6e9db943a33b74595
masker-Lee <masker-Lee@users.noreply.github.com>
2020-05-22 11:31:34 +0800

    update pollDB

--- app/schedule/pollIndexSchedule.js
+++ app/schedule/pollIndexSchedule.js
@@ -10,9 +10,10 @@ var lastId = undefined
 let schedulePollIndex = () => {
 	pollIndex();
 	let rule = new schedule.RecurrenceRule();
-	// rule.minute = [1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51]; //fetch pollIndex every 5 mins
+	// rule.minute = [1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51, 56]; //fetch pollIndex every 5 mins
 	rule.second = [1];
 	schedule.scheduleJob(rule, () => {
+		//the first poll id is 924993
 		if(lastId > 924993 || lastId == undefined)
 			pollIndexAll(lastId);
 		pollIndex();
